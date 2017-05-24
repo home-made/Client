aacdb92358583c476bdfadff82df3a4858333a35
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-24 05:02:59 -0700

    Save changes to origin master
    Missing AirGoogleMaps dir from xcode in origin master

--- App/Components/ChefMap.js
+++ App/Components/ChefMap.js
@@ -1,47 +1,57 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Dimensions,
+  Text,
+  View
+} from 'react-native';
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+
 
 
 export default class ChefMap extends Component {
-  state = {
-    mapRegion: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
-    }
-  };
-
-  componentWillMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.setState({
-        mapRegion: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-          error: null
-        }
-      });
-    });
-  }
 
   render() {
     return (
-      <MapView
-        style={{ alignSelf: "stretch", height: 700 }}
-        region={this.state.mapRegion}
-      />
+      <View style={{ flex: 1 }}>
+
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={{
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            }}
+          />
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
+    ...StyleSheet.absoluteFillObject,
+    top: 100,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map: {
+     ...StyleSheet.absoluteFillObject,
+  },
 });
+
